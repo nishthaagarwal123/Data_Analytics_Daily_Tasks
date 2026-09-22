ddc4c71fe1b32ec1f87582058fecd461dd0072a4
nishthaagarwal123 <nishthaagarwal2006@gmail.com>
2026-09-22 20:55:39 +0530

    Add files via upload

---
+++ IIOT_Daily_Task_Details-main/SQL/Day-1 SQL_Table_Overview.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE College1;
+USE College1;
+
+CREATE TABLE Students (
+    Student_ID INT,
+    Name VARCHAR(50),
+    Course VARCHAR(20),
+    Age INT
+);
+
+
+SELECT * FROM Students;
